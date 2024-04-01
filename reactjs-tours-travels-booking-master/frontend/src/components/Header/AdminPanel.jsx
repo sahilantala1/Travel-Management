92c0be2c0bf0ce11a1ad8f00920838f78e8f1ecf
sahilantala1 <sahilantala16@gmail.com>
2024-04-01 18:03:14 +0530

    create List Tour Page

--- reactjs-tours-travels-booking-master/frontend/src/components/Header/AdminPanel.jsx
+++ reactjs-tours-travels-booking-master/frontend/src/components/Header/AdminPanel.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
-import Tours from "../../pages/Tours";
+import ListTour from "./ListTour";
+// import Tours from "../../pages/Tours";
 
 const AdminPanel = ({ username }) => {
-  const handleButtonClick = (buttonName) => {};
+  // State to control the visibility of the Tours component
+  const [showTours, setShowTours] = useState(false);
 
-  const listData = () => {
-    return <></>;
+  const handleButtonClick = (buttonName) => {
+    // Handle button click based on the buttonName if needed
+    if (buttonName === "listTours") {
+      setShowTours(true); // Show Tours component when List Tours button is clicked
+    }
   };
 
   return (
     <>
-      <h1 className="h1 text-center ">Admin</h1>
+      <h1 className="h1 text-center">Admin</h1>
       <div className="container admin-panel">
         <div className="left-panel">
           <button
-            className="btn btn-primary m-2 "
+            className="btn btn-primary m-2"
             onClick={() => handleButtonClick("listUsers")}
           >
             List Users
           </button>
-          <button className="btn btn-primary m-2" onClick={() => listData()}>
+          <button
+            className="btn btn-primary m-2"
+            onClick={() => handleButtonClick("listTours")}
+          >
             List Tours
           </button>
           <button
@@ -30,6 +38,9 @@ const AdminPanel = ({ username }) => {
           </button>
         </div>
       </div>
+
+      {/* Render Tours component if showTours state is true */}
+      {showTours && <ListTour />}
     </>
   );
 };
