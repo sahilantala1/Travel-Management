5ab234a5c66c51096c8fac9748e34664d74a8ff8
sahilantala1 <sahilantala16@gmail.com>
2024-04-03 14:25:26 +0530

    Add Confirm Password

--- reactjs-tours-travels-booking-master/frontend/src/pages/Register.jsx
+++ reactjs-tours-travels-booking-master/frontend/src/pages/Register.jsx
@@ -1,3 +1,113 @@
+// import React, { useState, useContext } from "react";
+// import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+// import "../styles/login.css";
+// import { Link, useNavigate } from "react-router-dom";
+// import registerImg from "../assets/images/login.png";
+// import userIcon from "../assets/images/user.png";
+// import { AuthContext } from "../context/AuthContext";
+// import { BASE_URL } from "../utils/config";
+
+// const Register = () => {
+//   const [credentials, setCredentials] = useState({
+//     userName: undefined,
+//     email: undefined,
+//     password: undefined,
+//   });
+
+//   const { dispatch } = useContext(AuthContext);
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+//   };
+
+//   const handleClick = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const res = await fetch(`${BASE_URL}/auth/register`, {
+//         method: "post",
+//         headers: {
+//           "content-type": "application/json",
+//         },
+//         body: JSON.stringify(credentials),
+//       });
+//       const result = await res.json();
+
+//       if (!res.ok) alert(result.message);
+
+//       dispatch({ type: "REGISTER_SUCCESS" });
+//       navigate("/login");
+//     } catch (err) {
+//       alert(err.message);
+//     }
+//   };
+
+//   return (
+//     <section>
+//       <Container>
+//         <Row>
+//           <Col lg="8" className="m-auto">
+//             <div className="login__container d-flex justify-content-between">
+//               <div className="login__img">
+//                 <img src={registerImg} alt="" />
+//               </div>
+
+//               <div className="login__form">
+//                 <div className="user">
+//                   <img src={userIcon} alt="" />
+//                 </div>
+//                 <h2>Register</h2>
+
+//                 <Form onSubmit={handleClick}>
+//                   <FormGroup>
+//                     <input
+//                       type="text"
+//                       placeholder="Username"
+//                       id="username"
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </FormGroup>
+//                   <FormGroup>
+//                     <input
+//                       type="email"
+//                       placeholder="Email"
+//                       id="email"
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </FormGroup>
+//                   <FormGroup>
+//                     <input
+//                       type="password"
+//                       placeholder="Password"
+//                       id="password"
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </FormGroup>
+//                   <Button
+//                     className="btn secondary__btn auth__btn"
+//                     type="submit"
+//                   >
+//                     Create Account
+//                   </Button>
+//                 </Form>
+//                 <p>
+//                   Already have an account? <Link to="/login">Login</Link>
+//                 </p>
+//               </div>
+//             </div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </section>
+//   );
+// };
+
+// export default Register;
+
 import React, { useState, useContext } from "react";
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
 import "../styles/login.css";
@@ -12,6 +122,7 @@ const Register = () => {
     userName: undefined,
     email: undefined,
     password: undefined,
+    // confirmPassword:undefined,    // id
   });
 
   const { dispatch } = useContext(AuthContext);
@@ -23,6 +134,10 @@ const Register = () => {
 
   const handleClick = async (e) => {
     e.preventDefault();
+    if (credentials.password !== credentials.confirmPassword) {
+      alert("Password and confirm password do not match.");
+      return;
+    }
 
     try {
       const res = await fetch(`${BASE_URL}/auth/register`, {
@@ -83,6 +198,15 @@ const Register = () => {
                       type="password"
                       placeholder="Password"
                       id="password"
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="password"
+                      placeholder="Confirm Password"
+                      id="confirmPassword"
                       onChange={handleChange}
                       required
                     />
