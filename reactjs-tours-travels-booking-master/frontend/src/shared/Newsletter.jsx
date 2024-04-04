40f0d9ea3e3a4a928082dd692c5a72a95d767f8e
RAJESHSOJITRA <raj648patel@gmail.com>
2024-04-04 15:37:00 +0530

    text changed

--- reactjs-tours-travels-booking-master/frontend/src/shared/Newsletter.jsx
+++ reactjs-tours-travels-booking-master/frontend/src/shared/Newsletter.jsx
@@ -16,8 +16,7 @@ const NewsLetter = () => {
                         <input type="email" placeholder='Enter your email' />
                         <button className="btn newsletter__btn">Subcribe</button>
                      </div>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Obcaecati adipisici sunt in, provident facere ipsam?
+                     <p>Subscribe now for insider travel tips and destination guides, insider secrets, ensuring every adventure exceeds your wildest dreams.
                      </p>
                   </div>
                </Col>
